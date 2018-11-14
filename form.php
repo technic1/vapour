101e9212e0e51a3df339012bf33f1400756bef35
technic1 <dps_96@mail.ru>
2018-11-14 03:11:26 +0300

    excel test

--- form.php
+++ form.php
@@ -1,9 +1,20 @@
 <?php
-	$a = $_GET['text1'];
-	$b = $_GET['text2'];
+	
+	require 'vendor/autoload.php';
+
+	use PhpOffice\PhpSpreadsheet\Spreadsheet;
+	use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+
+	$station_power = $_GET['text1'];
+	$spreadsheet = new Spreadsheet();
+	$sheet = $spreadsheet->getActiveSheet();
+	$sheet->setCellValue('A1', $station_power);
+
+	$writer = new Xlsx($spreadsheet);
+	$writer->save('vapour_base.xlsx');	
 ?>
 
 <?php 
-	echo "$a<br/><br/>";
-	echo "$b";
+	echo "$station_power<br/><br/>";
 ?>
