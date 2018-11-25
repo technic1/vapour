13ecf6f26de45168b4579ba6bf992eaeeecfb201
technic1 <dps_96@mail.ru>
2018-11-26 01:30:38 +0300

    sql vapour

--- form.php
+++ form.php
@@ -1,20 +1,39 @@
 <?php
 	
-	require 'vendor/autoload.php';
+	$connection = mysqli_connect('localhost','root','','vapour');
+	if ($connection == false)
+	{
+		echo "Error<br>";
+		echo mysqli_connection_error();
+		exit();
+	}
 
+	$result = mysqli_query($connection, "SELECT * FROM stations");
+	while(mysqli_fetch_assoc($result));
+	{
+		print_r($result);
+		echo '<hr>';
+	}	
+/*	require_once 'vendor/autoload.php';
+	require_once 'C:\xampp\htdocs\vapour\vendor\phpoffice\phpspreadsheet\src\PhpSpreadsheet\IOFactory.php';
 	use PhpOffice\PhpSpreadsheet\Spreadsheet;
 	use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-
+	use PhpOffice\PhpSpreadsheet\IOFactory;
 
 	$station_power = $_GET['text1'];
-	$spreadsheet = new Spreadsheet();
-	$sheet = $spreadsheet->getActiveSheet();
-	$sheet->setCellValue('A1', $station_power);
+	$spreadsheet = \PhpOffice\PhpSpreadsheet\Reader\IOFactory::load("vapour_base.xlsx");
+*/
+/*
+	$station_power = $_GET['text1'];
+	$spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load("vapour_base.xlsx");
+	$spreadsheet->getActiveSheet()->setCellValue('A1', $station_power);
 
-	$writer = new Xlsx($spreadsheet);
-	$writer->save('vapour_base.xlsx');	
-?>
+	$writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet,"Xlsx");
+	$writer->getActiveSheet()->setCellValue('A1', $station_power);
+	$writer->save("vapour_base.xlsx");	
+?>	
 
 <?php 
 	echo "$station_power<br/><br/>";
 ?>
+*/
---
+++ vapour.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Ноя 25 2018 г., 23:29
+-- Версия сервера: 10.1.36-MariaDB
+-- Версия PHP: 7.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `vapour`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `stations`
+--
+
+CREATE TABLE `stations` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `power` float DEFAULT NULL,
+  `open` int(11) DEFAULT NULL,
+  `container` int(11) DEFAULT NULL,
+  `installation` int(11) DEFAULT NULL,
+  `shipping` int(11) DEFAULT NULL,
+  `mode` varchar(255) NOT NULL,
+  `scheme` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `stations`
+--
+ALTER TABLE `stations`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
